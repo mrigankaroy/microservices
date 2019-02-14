da0895e6a4d8fc3c5ad25c059c1d3b0f49a180a7
mrigankaroy <mriganka.shekharroy@yahoo.co.in>
2019-02-14 14:31:02 +0530

    Mongo configuration

--- gateway-engine/conf/conf_data/load_gateway_conf.sh
+++ gateway-engine/conf/conf_data/load_gateway_conf.sh
@@ -1,1 +1,1 @@
-mongo --eval "var config_mongo_user='microservice_admin'; var config_mongo_password='microservice_admin'; var config_server_mongo='config-server';" /conf_data/config-server-data.js
+mongo --eval "var config_mongo_user='microservice_admin'; var config_mongo_password='microservice_admin'; var config_server_mongo='config-server';" /conf_data/gateway-engine/config-server-data.js
--- gateway-engine/conf/predeployment.sh
+++ gateway-engine/conf/predeployment.sh
@@ -1,8 +1,11 @@
+echo "Make directory /conf_data/gateway-engine inside config-server-mongo"
+docker exec -it config-server-mongo mkdir /conf_data/gateway-engine
+
 echo "Copy /conf_data/config-server-data.js to config-server-mongo"
-docker cp ./conf_data/config-server-data.js config-server-mongo:/conf_data/config-server-data.js
+docker cp ./conf_data/config-server-data.js config-server-mongo:/conf_data/gateway-engine/config-server-data.js
 
 echo "Copy /conf_data/load_gateway_conf.sh config-server-mongo"
-docker cp ./conf_data/load_gateway_conf.sh config-server-mongo:/conf_data/load_gateway_conf.sh
+docker cp ./conf_data/load_gateway_conf.sh config-server-mongo:/conf_data/gateway-engine/load_gateway_conf.sh
 
 echo "Execute /conf_data/load_gateway_conf.sh"
-docker exec -it config-server-mongo bash /conf_data/load_gateway_conf.sh
+docker exec -it config-server-mongo bash /conf_data/gateway-engine/load_gateway_conf.sh
